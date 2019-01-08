0a9b07b6682c9b95cce900a1d55efffa4cf7d04c
mayur <mayur@DESKTOP-D80A036>
2019-01-08 11:02:13 +0000

    Test Commit

---
+++ TP12/src/main/java/TP1/TP12/App.java
@@ -0,0 +1,13 @@
+package TP1.TP12;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TP12/src/main/java/TP1/TP12/TestClass1.java
@@ -0,0 +1,10 @@
+package TP1.TP12;
+
+public class TestClass1 {
+
+	public TestClass1() {
+		System.out.println("Test");
+	}
+
+	
+}
